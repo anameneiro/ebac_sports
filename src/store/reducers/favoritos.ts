61fe14cf4c931ed6cb6f278142894e9088062f0f
Ana Claudia <amoacharlo@gmail.com>
2024-03-17 02:38:39 -0300

    Altera o gerenciamento de estado de "useState" para um gerenciamento com Redux.

---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Produto } from '../../App'
+
+type FavoritoState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritoState = {
+  itens: []
+}
+
+const favoritoSlice = createSlice({
+  name: 'favorito',
+  initialState,
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const favorito = action.payload
+
+      if (state.itens.find((p) => p.id === favorito.id)) {
+        alert('Item já foi favoritado!')
+      } else {
+        state.itens.push(favorito)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritoSlice.actions
+export default favoritoSlice.reducer
